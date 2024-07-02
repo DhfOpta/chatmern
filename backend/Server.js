9216236705fb3302db0a73641298fe20d5ea067a
Tirth-Parikh <tirthparikh2203@gmail.com>
2024-07-02 13:38:01 +0530

    server2

--- backend/Server.js
+++ backend/Server.js
@@ -97,7 +97,7 @@ console.log(tokn, 'tokntokntokntokn jon');
 
 app.get('/auth/google', passport.authenticate("google", { scope: ['profile', "email"] }))
 app.get('/auth/google/callback', passport.authenticate("google", {
-    successRedirect: "/authDas/userDashboard",
+    successRedirect: "https://chatmern-281.onrender.com/authDas/userDashboard",
     failureRedirect: "https://chatmern-281.onrender.com"
 }))
  
